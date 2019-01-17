1ab0a525da71a301a6f0ee552eb9d4438a00c34f
boxsmi <boxsmi@gmail.com>
2019-01-17 14:30:19 +0300

    Обновлен класс QuestionAndUnswers (Вопрос-Ответ)

--- src/dns/QuestionAndAnswers.java
+++ src/dns/QuestionAndAnswers.java
@@ -3,26 +3,42 @@ Created by Aleksandr
                     Smirnov*/
 package dns;
 
+import java.util.Date;
+
 public class QuestionAndAnswers {
 
-    private String themeOfQuestion;
+    private String [] themeOfQuestion;
     private String question;
-    private String author;
+    private String authorOfQuestion;
+    private String emailauthorOfQuestion;
+    private Date dataQuestion;
+    private String zagolovokOfQuestion;
+    private String authorOfAnswer;
+    private String emailauthorOfAnswer;
+    private Date dataAnswer;
+    private String answer;
 
     QuestionAndAnswers() {
     }
 
-    public QuestionAndAnswers(String themeOfQuestion, String question, String author) {
+    public QuestionAndAnswers(String[] themeOfQuestion, String question, String authorOfQuestion, String emailauthorOfQuestion, Date dataQuestion, String zagolovokOfQuestion, String authorOfAnswer, String emailauthorOfAnswer, Date dataAnswer, String answer) {
         this.themeOfQuestion = themeOfQuestion;
         this.question = question;
-        this.author = author;
+        this.authorOfQuestion = authorOfQuestion;
+        this.emailauthorOfQuestion = emailauthorOfQuestion;
+        this.dataQuestion = dataQuestion;
+        this.zagolovokOfQuestion = zagolovokOfQuestion;
+        this.authorOfAnswer = authorOfAnswer;
+        this.emailauthorOfAnswer = emailauthorOfAnswer;
+        this.dataAnswer = dataAnswer;
+        this.answer = answer;
     }
 
-    public String getThemeOfQuestion() {
+    public String[] getThemeOfQuestion() {
         return themeOfQuestion;
     }
 
-    public void setThemeOfQuestion(String themeOfQuestion) {
+    public void setThemeOfQuestion(String[] themeOfQuestion) {
         this.themeOfQuestion = themeOfQuestion;
     }
 
@@ -34,20 +50,67 @@ public class QuestionAndAnswers {
         this.question = question;
     }
 
-    public String getAuthor() {
-        return author;
+    public String getAuthorOfQuestion() {
+        return authorOfQuestion;
     }
 
-    public void setAuthor(String author) {
-        this.author = author;
+    public void setAuthorOfQuestion(String authorOfQuestion) {
+        this.authorOfQuestion = authorOfQuestion;
     }
 
-    @Override
-    public String toString() {
-        return "QuestionAndAnswers{" +
-                "themeOfQuestion='" + themeOfQuestion + '\'' +
-                ", question='" + question + '\'' +
-                ", author='" + author + '\'' +
-                '}';
+    public String getEmailauthorOfQuestion() {
+        return emailauthorOfQuestion;
+    }
+
+    public void setEmailauthorOfQuestion(String emailauthorOfQuestion) {
+        this.emailauthorOfQuestion = emailauthorOfQuestion;
+    }
+
+    public Date getDataQuestion() {
+        return dataQuestion;
+    }
+
+    public void setDataQuestion(Date dataQuestion) {
+        this.dataQuestion = dataQuestion;
+    }
+
+    public String getZagolovokOfQuestion() {
+        return zagolovokOfQuestion;
+    }
+
+    public void setZagolovokOfQuestion(String zagolovokOfQuestion) {
+        this.zagolovokOfQuestion = zagolovokOfQuestion;
+    }
+
+    public String getAuthorOfAnswer() {
+        return authorOfAnswer;
+    }
+
+    public void setAuthorOfAnswer(String authorOfAnswer) {
+        this.authorOfAnswer = authorOfAnswer;
+    }
+
+    public String getEmailauthorOfAnswer() {
+        return emailauthorOfAnswer;
+    }
+
+    public void setEmailauthorOfAnswer(String emailauthorOfAnswer) {
+        this.emailauthorOfAnswer = emailauthorOfAnswer;
+    }
+
+    public Date getDataAnswer() {
+        return dataAnswer;
+    }
+
+    public void setDataAnswer(Date dataAnswer) {
+        this.dataAnswer = dataAnswer;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(String answer) {
+        this.answer = answer;
     }
 }
