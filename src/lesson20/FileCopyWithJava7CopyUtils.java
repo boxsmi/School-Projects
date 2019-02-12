c25204df3ffa720fda6b47c748cef0f4d9a820f2
boxsmi <boxsmi@gmail.com>
2019-02-12 12:09:43 +0300

    реализация метода копирования с помощью Java 7 Copy Utils

--- src/lesson20/FileCopyWithJava7CopyUtils.java
+++ src/lesson20/FileCopyWithJava7CopyUtils.java
@@ -3,5 +3,28 @@ Created by Aleksandr
                     Smirnov*/
 package lesson20;
 
-public class FileCopyWithJava7CopyUtils {
+import Interfaces.FileAlreadyExistsException;
+import Interfaces.FileCopyFailedException;
+import Interfaces.FileCopyUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class FileCopyWithJava7CopyUtils implements FileCopyUtils {
+    @Override
+    public void copyFile(String source, String destination) throws FileCopyFailedException, FileAlreadyExistsException, IOException {
+        File destinationFile = new File(destination);
+        if (destinationFile.exists()) throw new FileAlreadyExistsException("File Already Exists");
+        Path sour = Paths.get(source);
+        Path dest = Paths.get(destination);
+        try {
+            Files.copy(sour, dest);
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new FileCopyFailedException(e.getMessage());
+        }
+    }
 }
