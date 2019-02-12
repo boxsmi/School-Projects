21429ce58776cedbd3141881f5e10cfe994528c6
boxsmi <boxsmi@gmail.com>
2019-02-12 12:12:10 +0300

    реализация метода копирования с помощью Standart Utils

--- src/lesson20/FileCopyWithStandartUtils.java
+++ src/lesson20/FileCopyWithStandartUtils.java
@@ -3,5 +3,29 @@ Created by Aleksandr
                     Smirnov*/
 package lesson20;
 
-public class FileCopyWithStandartUtils {
+import Interfaces.FileAlreadyExistsException;
+import Interfaces.FileCopyFailedException;
+import Interfaces.FileCopyUtils;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class FileCopyWithStandartUtils implements FileCopyUtils {
+    @Override
+    public void copyFile(String source, String destination) throws FileCopyFailedException, FileAlreadyExistsException, IOException {
+        File destinationFile = new File(destination);
+        if (destinationFile.exists()) throw new FileAlreadyExistsException("File Already Exists");
+        try {
+            FileInputStream sour = new FileInputStream(source);
+            FileOutputStream dest = new FileOutputStream(destination);
+            byte[] buffer = new byte[sour.available()];
+            sour.read(buffer, 0, buffer.length);
+            dest.write(buffer, 0, buffer.length);
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new FileCopyFailedException(e.getMessage());
+        }
+    }
 }
